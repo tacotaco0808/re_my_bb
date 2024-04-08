f2cb6059200d96a7f151956f6fdc7090c761497b
takutou <takuto1488@gmail.com>
2024-04-08 18:28:05 +0900

    react02まで

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,12 @@
+'use client'
+import style from './Header.module.css'
+function Header() {
+  return (
+    <>
+      <header>
+        <h1 className={style.title}>掲示板だよ</h1>
+      </header>
+    </>
+  )
+}
+export default Header
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import { useEffect, useState } from 'react'
 import Threads from './components/Threads'
+import Header from './components/Header'
 
 export default function Home() {
   return (
     <>
+      <Header />
       <Threads />
     </>
   )
