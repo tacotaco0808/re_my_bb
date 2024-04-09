6ea4c9e8663a87bcfc3b1cbb38dd1bcba4d824d2
takutou <takuto1488@gmail.com>
2024-04-09 18:58:22 +0900

    スタイルを中央ぞろえ

--- src/app/thread/new/page.tsx
+++ src/app/thread/new/page.tsx
@@ -1,10 +1,11 @@
 import Header from '@/app/components/Header'
 import CreateThread from '@/app/components/CreateThread'
+import style from './page.module.css'
 export default function New() {
   return (
     <>
       <Header />
-      <main>
+      <main className={style.main}>
         <CreateThread />
       </main>
     </>
